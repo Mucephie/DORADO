7ce04e835a73d6d8a566d299405705e2d473d2f8
Joshua Parsons <mucephie@outlook.com>
2019-07-10 01:02:29 -0400

    Major reduction in code size for image reduction script

--- im_reduc.py
+++ im_reduc.py
@@ -14,82 +14,33 @@ FLAT_prefix = 'FLAT_'
 FLAT_suffix = '.FLAT'
 filters = ['BLUE', 'CLEAR', 'EIGHT', 'GREEN', 'LUMINANCE', 'RED', 'SEVEN', 'SIX']
 
-
-
-# # IRAF made master bias image
-# master_bias_header, master_bias_data = get_im(file_path, 'MasterBias')
-# plt_fits(master_bias_data)
-
 # master bias made by DRACO
 master_bias = draco.mast_reduc(file_path + 'Bias/', im_prefix, BIAS_suffix, 10)
 #fits.writeto('master_bias.fits', master_bias)
-#plt_fits(master_bias)
 
 reduc_filters =  ['BLUE', 'CLEAR', 'LUMINANCE', 'SEVEN', 'SIX']
+size = master_bias.shape
+im_count = 5
+master_i_Flat = np.zeros((size[0], size[1], len(reduc_filters)))
 
-# master BLUE Flat made by DRACO
-master_BLUE_Flat = draco.mast_reduc(file_path + 'Flats/BLUE/', im_prefix + 'BLUE-', FLAT_suffix, 10)
-master_BLUE_Flat = draco.imarith(master_BLUE_Flat, '-', master_bias)
-master_BLUE_Flat = draco.norm_flat(master_BLUE_Flat)
-#fits.writeto('master_BLUE_Flat.fits', master_BLUE_Flat)
+for i in range(len(reduc_filters)):
+    master_i_Flat[:, :, i] = draco.mast_reduc(file_path + 'Flats/' + reduc_filters[i] + '/', im_prefix + reduc_filters[i] + '-', FLAT_suffix, 10)
+    master_i_Flat[:, :, i] = draco.imarith(master_i_Flat[:, :, i], '-', master_bias)
+    master_i_Flat[:, :, i] = draco.norm_flat(master_i_Flat[:, :, i])
+    #fits.writeto('master_' + reduc_filters[i] + '_Flat.fits', master_i_Flat[i])
 
-# master CLEAR Flat made by DRACO
-master_CLEAR_Flat = draco.mast_reduc(file_path + 'Flats/CLEAR/', im_prefix + 'CLEAR-', FLAT_suffix, 10)
-master_CLEAR_Flat = draco.imarith(master_CLEAR_Flat, '-', master_bias)
-master_CLEAR_Flat = draco.norm_flat(master_CLEAR_Flat)
-#fits.writeto('master_CLEAR_Flat.fits', master_CLEAR_Flat)
+series_n = np.zeros((size[0], size[1], im_count, len(reduc_filters)))
+for n in  range(len(reduc_filters)):
+    series_n[:, :, :, n] = draco.get_series(file_path + 'Lights/', c46P_prefix + reduc_filters[n] + '-', '', 5)
+    series_n[:, :, :, n] = draco.series_arith(series_n[:, :, :, n],'-', master_bias, 5)
+    series_n[:, :, :, n] = draco.series_arith(series_n[:, :, :, n],'/', master_i_Flat[:, :, n], 5)
+    sky = draco.sky_est(series_n[:, :, :, n])
+    series_n[:, :, :, n] = draco.series_arith(series_n[:, :, :, n],'-', sky, 5)
+    #draco.series_write(series_n[:, :, :, n], 'c46P-' + reduc_filters[n] + '-',5)
 
-# master LUMINANCE Flat made by DRACO
-master_LUM_Flat = draco.mast_reduc(file_path + 'Flats/LUMINANCE/', im_prefix + 'LUMINANCE-', FLAT_suffix, 10)
-master_LUM_Flat = draco.imarith(master_LUM_Flat, '-', master_bias)
-master_LUM_Flat = draco.norm_flat(master_LUM_Flat)
-#fits.writeto('master_LUM_Flat.fits', master_LUM_Flat)
-
-# master SEVEN Flat made by DRACO
-master_SEVEN_Flat = draco.mast_reduc(file_path + 'Flats/SEVEN/', im_prefix + 'SEVEN-', FLAT_suffix, 10)
-master_SEVEN_Flat = draco.imarith(master_SEVEN_Flat, '-', master_bias)
-master_SEVEN_Flat = draco.norm_flat(master_SEVEN_Flat)
-#fits.writeto('master_SEVEN_Flat.fits', master_SEVEN_Flat)
-
-# master SIX Flat made by DRACO
-master_SIX_Flat = draco.mast_reduc(file_path + 'Flats/SIX/', im_prefix + 'SIX-', FLAT_suffix, 10)
-master_SIX_Flat = draco.imarith(master_SIX_Flat, '-', master_bias)
-master_SIX_Flat = draco.norm_flat(master_SIX_Flat)
-#fits.writeto('master_SIX_Flat.fits', master_SIX_Flat)
+draco.plt_fits(series_n[:, :, 3, 3])
 
 
-
-
-
-# master BLUE light made by DRACO
-series_BLUE = draco.get_series(file_path + 'Lights/', c46P_prefix + 'BLUE-', '', 5)
-series_BLUE = draco.series_arith(series_BLUE,'-', master_bias, 5)
-series_BLUE = draco.series_arith(series_BLUE,'/', master_BLUE_Flat, 5)
-series_BLUE = draco.series_arith(series_BLUE,'-', 54, 5)
-#draco.series_write(series_BLUE, 'c46P-BLUE-SKY-',5)
-draco.plt_fits(series_BLUE[:, :, 3])
-# master CLEAR light made by DRACO
-series_CLEAR = draco.get_series(file_path + 'Lights/', c46P_prefix + 'CLEAR-', '', 5)
-series_CLEAR = draco.series_arith(series_CLEAR,'-', master_bias, 5)
-series_CLEAR = draco.series_arith(series_CLEAR,'/', master_CLEAR_Flat, 5)
-#draco.series_write(series_CLEAR, 'c46P-CLEAR-',5)
-# master LUMINANCE light made by DRACO
-series_LUM = draco.get_series(file_path + 'Lights/', c46P_prefix + 'LUM-', '', 5)
-series_LUM = draco.series_arith(series_LUM,'-', master_bias, 5)
-series_LUM = draco.series_arith(series_LUM,'/', master_LUM_Flat, 5)
-#draco.series_write(series_LUM, 'c46P-LUM-',5)
-# master SEVEN light made by DRACO
-series_SEVEN = draco.get_series(file_path + 'Lights/', c46P_prefix + 'SEVEN-', '', 5)
-series_SEVEN = draco.series_arith(series_SEVEN,'-', master_bias, 5)
-series_SEVEN = draco.series_arith(series_SEVEN,'/', master_SEVEN_Flat, 5)
-#draco.draco.series_write(series_SEVEN, 'c46P-SEVEN-',5)
-# master SIX light made by DRACO
-series_SIX = draco.get_series(file_path + 'Lights/', c46P_prefix + 'SIX-', '', 5)
-series_SIX = draco.series_arith(series_SIX,'-', master_bias, 5)
-series_SIX = draco.series_arith(series_SIX,'/', master_SIX_Flat, 5)
-#draco.series_write(series_SIX, 'c46P-SIX-',5)
-
-#draco.plt_fits(master_BLUE)
 
 
 

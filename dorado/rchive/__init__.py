1e6b7a194acecc4be979a548f5fd2ef30c9d9008
Joshua Parsons <mucephie@outlook.com>
2020-09-14 15:39:55 -0400

    Updated package indexing

--- dorado/rchive/__init__.py
+++ dorado/rchive/__init__.py
@@ -3,3 +3,8 @@ This is the docstring for the examplesubpkg package.  Normally you would
 have whatever.py files in this directory implementing some modules, but this
 is just an example sub-package, so it doesn't actually do anything.
 """
+__all__ = []
+
+from .rchive import *
+
+__all__ += rchive.__all__

a0902407e6ddc4aeafda77d95494f96b9abcd477
Joshua Parsons <mucephie@outlook.com>
2020-11-23 22:36:15 -0500

    modified filestring in platesolve setup

--- dorado/examples/proc.py
+++ dorado/examples/proc.py
@@ -1,8 +1,6 @@
 import dorado
-
-
 import astroalign as aa
-# from donuts import Donuts
+
 
 ## Organize and prepare your data
 # Get a fits file formated date string for the current observing night.
@@ -51,7 +49,7 @@ dorado.write_series(directory, series, target)
 data = series[0].data
 # Set up a file string for dorado to locate image
 expath = '/wrk/calibrated/' 
-im = target +  '_0.calibrated.fit'
+im = target +  '-0_c.fit'
 image_file_path = dorado.file_string(directory + expath, im)
 # Pass the data and file string to dorado
 solved = dorado.plate_solve(data, image_file_path, write_fits = True, write_name = target + '_solved')
@@ -66,6 +64,7 @@ for image in series:
     aa_series.append(img_r)
 
 dorado.write_series(directory + '/wrk/aligned/', aa_series, target + '_a')
+# You now have a fully calibrated and aligned data set with WCS data
 
 
 
@@ -75,26 +74,3 @@ dorado.write_series(directory + '/wrk/aligned/', aa_series, target + '_a')
 
 
 
-
-
-
-# # Align each image with the solved image
-# p, (pos_0, pos_1) = astroalign.find_transform(series[0].data, series[20].data)
-
-# for (x1, y1), (x2, y2) in zip(pos_0, pos_1):
-#     print("({:.2f}, {:.2f}) in source --> ({:.2f}, {:.2f}) in target"
-#           .format(x1, y1, x2, y2))
-
-# # Set up a reference image
-# refim = lights_list[0]
-# d = Donuts(refimage = refim, image_ext = 0, subtract_bkg = True, ntiles = 64)
-# # for each image, compute the x/y translation required to align the images onto the reference image
-# xp, yp = [], []
-# for image in lights_list:
-#     shift_result = d.measure_shift(image)
-#     x, y = shift_result.x, shift_result.y
-#     # y = shift_result.y
-#     xp.append(x.value)
-#     yp.append(y.value)
-#     # Also check out shift_result.sky_background
-#     # print(x, y)

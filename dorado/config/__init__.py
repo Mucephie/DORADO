4eedec124bd0941ae7c01456d04f3d6d3a0f1d9f
Joshua Parsons <mucephie@outlook.com>
2021-02-03 20:54:25 -0500

    More work on initializing config system

--- dorado/config/__init__.py
+++ dorado/config/__init__.py
@@ -7,10 +7,16 @@ from astropy import config as _config
 
 class ConfigNamespace(_config.ConfigNamespace):
     rootname = 'dorado'
+    config_dir = _config.get_config_dir(rootname)
+    _config.get_config(packageormod = rootname, rootname = rootname)
     api_key = _config.ConfigItem(
         '',
         "The Astrometry.net API key."
     )
+    use_logger = _config.ConfigItem(
+        True, 'Whether to use the Dorado logger system.')
+    # _config.generate_config(pkgname = rootname)
+
 
 class ConfigItem(_config.ConfigItem):
     rootname = 'dorado'
@@ -21,5 +27,6 @@ class ConfigItem(_config.ConfigItem):
 conf = ConfigNamespace()
 
 
-print('Dorado directory created: ', _config.get_config_dir('dorado'))
+# print('Dorado directory created: ', _config.get_config_dir('dorado'))
 
+__all__ = ['conf']

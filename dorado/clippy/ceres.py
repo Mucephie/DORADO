99607d48adb3f7570f4dfd73f72b78ed38d786bd
Joshua Parsons <mucephie@outlook.com>
2021-03-23 22:39:25 -0400

    Added better logic to init

--- dorado/clippy/ceres.py
+++ dorado/clippy/ceres.py
@@ -181,22 +181,39 @@ class zellars:
 
 class Stack:
     def __init__(self, data, filter = '', times = [], calibrated = None, aligned = None, target = ''):
+    def __init__(self, data, flat = None, filter = '', times = [], calibrated = None, aligned = None, target = None):
         self.data = data
+        self.flat = flat
         self.filter = filter
         self.length = len(data)
         self.calibrated = calibrated
         self.aligned = aligned
+
         self.target = target
         self.target_info = {} # dictionary of values
         self.times = times # put together a check for if filled, if not, try to find it.
+        self.target_info = {} # dictionary of values, call simbad?
 
+        self.times = times 
+        self.wcs = None
         # include things like flux uncertainty etc.
         # include wcs
 
+        # save data
+        # if flat is none, call clippy for help
 
         if filter == '':
+        if self.filter == '':
             try:
                 filter = data[0].header['filter']
+                self.filter = data[0].header['filter']
+            except:
+                self.filter = ''
+        
+        if self.times == []:
+            try: 
+                self.get_times()
+                print('Times set')
             except:
                 filter = ''
         

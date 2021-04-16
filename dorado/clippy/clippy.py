2c61bb80217ce7c362660a1fe3b28beb8b7e6195
Joshua Parsons <mucephie@outlook.com>
2021-04-16 19:29:29 -0400

    Immense growth of functions and class

--- dorado/clippy/clippy.py
+++ dorado/clippy/clippy.py
@@ -2,14 +2,14 @@ import warnings
 warnings.filterwarnings('ignore')
 
 from astropy.nddata.ccddata import CCDData
-from ceres import Ceres, Stack
-from ceres import Ceres, Stack, zellars
+from .ceres import Ceres
+from .Stack import Stack
+from .Zellars import zellars
 import numpy as np
 from astropy import config as _config
 from astropy.utils.misc import isiterable
 import astropy.units as un
 from astropy.time import Time
-from ccdproc import ImageFileCollection
 import ccdproc
 
 from astroquery.astrometry_net import AstrometryNet
@@ -21,9 +21,7 @@ import datetime
 from pathlib import Path
 
 
-
 ast = AstrometryNet()
-ast.api_key = 'jefjaaeekvrszphp'
 
 '''
 Clippy is the handler of the Dorado system,
@@ -49,19 +47,8 @@ class clippy:
         self.init_dir()
         self.unit = un.adu
 
-    def iniWt_dir(self):
     def init_dir(self):
         self.enter_dordir()
-        os.makedirs('../data/wrk', exist_ok = True)
-        os.makedirs('../data/flats', exist_ok = True)
-        os.makedirs('../data/bias', exist_ok = True)
-        os.makedirs('../data/darks', exist_ok = True)
-        os.makedirs('../data/raw', exist_ok = True)
-        os.makedirs('../data/graphical', exist_ok = True)
-        os.makedirs('../data/projects', exist_ok = True)
-        os.makedirs('../targets', exist_ok = True)
-        os.makedirs('../logs', exist_ok = True)
-        os.makedirs('../cache', exist_ok = True)
         os.makedirs('./data/wrk', exist_ok = True)
         os.makedirs('./data/flats', exist_ok = True)
         os.makedirs('./data/bias', exist_ok = True)
@@ -112,8 +99,6 @@ class clippy:
     def exit_dordir(self):
         os.chdir(self.stardir)
 
-    def diread(self, date):
-        path = self.dordir / 'data' / 'raw' / date
     def diread(self, dirarray):
         if isiterable(dirarray):
             path = self.dordir
@@ -133,16 +118,13 @@ class clippy:
                     directories.append(entry)
         return files, directories
     
-    def dirscan(self, date):
-
-        files, directories = self.diread(date)
     def dirscan(self, dirarray):
         path = self.dordir
         for dir in dirarray:
             path = path / dir
         files, directories = self.diread(path)
 
-        biasstr = ['Bias', 'Bias', 'bias']
+        biasstr = ['Bias', 'Bias', 'bias', 'BIAS']
         flatsstr = ['FLAT', 'FlatField', 'flat', 'Flat', 'Flats', 'flats', 'FLATS', 'FlatFields']
         lightsstr = ['lights', 'Lights', 'LIGHTS']
 
@@ -152,20 +134,47 @@ class clippy:
 
             else:
                 print('Single directory level organization format detected.')
-                print('Reading files.')
                 # print('Reading files.')
                 # compile these into master frame and pass them to clippy
-                path = self.dordir / 'data' / 'raw' / date
                 # path = self.dordir / 'data' / 'raw' / date
-                # if np.any(for i in biasstr if i in s.name)
-                biasl = [s for s in files if s.name in biasstr]
-                bias = ImageFileCollection(location = path, filenames = biasl)
-                flatsl = [s for s in files if s.name in flatsstr]
-                flats = ImageFileCollection(location = path, filenames = flatsl)
+                biasl = []
+                for strbias in biasstr:
+                    for s in files:
+                        if (str(strbias)) in str(s.name):
+                            biasl.append(s)
+                bias = []
+                for i in range(len(biasl)):
+                    hdu = CCDData.read(biasl[i].path) #, unit = self.unit)
+                    bias.append(hdu)
+                print('Bias searched.')
+                flatsl = []
+                for strflat in flatsstr:
+                    for s in files:
+                        if strflat in s.name:
+                            flatsl.append(s)
+                flats = []
+                for i in flatsl:
+                    hdu = CCDData.read(i.path) #, unit = self.unit)
+                    flats.append(hdu)
+                print('flats searched.')
                 # strip into ceres (check if multi-filter)
-                lightsl = [s for s in files if (s.name not in flatsstr) and (s.name not in biasstr)]
-                lights = ImageFileCollection(location = path, filenames = lightsl)
-                
+                lightsl = []
+                nonlight = []
+                for b in biasl:
+                    nonlight.append(b.name)
+                for f in flatsl:
+                    nonlight.append(f.name)
+
+                for s in files:
+                    if (str(s.name) not in nonlight):
+                        lightsl.append(s)
+
+                # lightsl = [s for s in files if (s.name not in biasl) and (s.name not in flatsl)]
+                lights = []
+                for i in lightsl:
+                    hdu = CCDData.read(i.path) # , unit = self.unit)
+                    lights.append(hdu)
+                print('lights searched.')
                 return bias, flats, lights
 
 
@@ -193,13 +202,9 @@ class clippy:
                     else:
                         print('Single directory lights organization format detected.')
                         # print('Reading files.')
-                        lightsarr = []
                         lights = []
                         for i in files:
                             hdu = CCDData.read(i.path, unit = self.unit)
-                            lightsarr.append(hdu)
-                        filter = ImageFileCollection(ldir).values('filter', unique = True)
-                        lights = [filter, lightsarr]
                             lights.append(hdu)
                         # filter = ImageFileCollection(ldir).values('filter', unique = True)
                         # lights = [filter, lightsarr]
@@ -277,7 +282,6 @@ class clippy:
             bias = ccdproc.combine(biasIFC, method = 'average', unit = self.unit)
             bias.meta['stacked'] = True
             bias.header['numsubs'] = len(biasIFC)
-
             date = Time(bias.header['DATE-OBS'], format='fits').mjd
             bias.data = bias.data.astype('uint16') 
             fname = str(int(date)) + '_Bias.fits'
@@ -294,31 +298,37 @@ class clippy:
                 print('Bias for date already saved.')
             return bias
 
-    def mkceres(self, date):
-            biasIFC, flats, lights = clip.dirscan(date)
-    def mkceres(self, date, sub = 'raw'):
-            dirarray = ['data', sub, date]
+    def mkceres(self, date, sub = 'raw', target = None, calibrated = False, aligned = False):
+            if aligned:
+                dirarray = ['data', sub, date, 'aligned']
+            elif calibrated:
+                dirarray = ['data', sub, date, 'calibrated']
+            else:
+                dirarray = ['data', sub, date]
             biasIFC, flats, lights = clip.dirscan(dirarray)
-            flat = self.mkFlat(flats)
-            bias = self.mkBias(biasIFC)
+            print(len(flats), ' flats found.')
+            print(len(biasIFC), ' bias frames found.')
+            print(len(lights), ' lights found')
+            
+            
+
             # save these frames
-            time = [] # get_time() function
 
-            filter = lights[0][0]
-            lights = lights[1]
-            print(filter)
-            # filter = lights[0][0]
-            # print(filter)
 
-            cere = Ceres()
-            cere.bias = bias
-            cere.flats = flat
-            cere.add_stack(Stack(lights, filter = filter, calibrated = False), filter)
-            cere.time = Time(bias.header['DATE-OBS'], format='fits')
+            if len(biasIFC) == 0:
+                cere = Ceres(time = Time(lights[0].header['DATE-OBS'], format='fits'))
+            else:
+                bias = self.mkBias(biasIFC)
+                cere = Ceres(bias = bias, time = Time(lights[0].header['DATE-OBS'], format='fits'))
+
             # for f in filter_data:
             # cere.flats[flat.header['filter']] = flat
-            # filter = filter, 
-            cere.add_stack(Stack(lights[0:50], flat = flat, calibrated = False))
+
+            if len(flats) == 0:
+                 cere.add_stack(Stack(lights, calibrated = calibrated, aligned = aligned, target = target))
+            else:
+                flat = self.mkFlat(flats)
+                cere.add_stack(Stack(lights, flat = flat, calibrated = calibrated, aligned = aligned, target = target))
             
 
             return cere
@@ -402,67 +412,122 @@ class clippy:
             print('Solve failed! :(')
             return 
 
-clip = clippy()
+    def getDateString(self, cr):
+        day = str(cr.date.ymdhms['day'])
+        day2 = str(cr.date.ymdhms['day'] + 1)
+        month = str(cr.date.ymdhms['month'])
+        if cr.date.ymdhms['day'] < 10:
+            day = '0' + str(cr.date.ymdhms['day'])
+            if cr.date.ymdhms['day'] < 9:
+                day2 = '0' + str(cr.date.ymdhms['day'] + 1)
+        if cr.date.ymdhms['month'] < 10:
+            month = '0' + str(cr.date.ymdhms['month'])
 
-# 2020-11-04+05
+        datestr = str(cr.date.ymdhms['year']) + '-' + month + '-' + day + '+' + day2
+        cr.datestr = datestr
+
+    def mkwrk(self, cr):
+        if cr.datestr == None:
+            self.getDateString(cr)
+        datestr = cr.datestr
+        wrkdir = self.dordir / 'data' / 'wrk'
+        os.makedirs(wrkdir / datestr, exist_ok = True)
+        os.makedirs(wrkdir / datestr / 'aligned', exist_ok = True)
+        os.makedirs(wrkdir / datestr / 'calibrated', exist_ok = True)
+        os.makedirs(wrkdir / datestr / 'uncalibrated', exist_ok = True)
+        os.makedirs(wrkdir / datestr / 'WCS', exist_ok = True)
+        # figures, targets, log, omitted images, observation metadata
+        
+    def savewrk(self, cr, filters = None):
+        wrkdir = self.dordir / 'data' / 'wrk'
+        if cr.datestr == None:
+            self.getDateString(cr)
+        datestr = cr.datestr
+        # day = str(cr.date.ymdhms['day'])
+        # day2 = str(cr.date.ymdhms['day'] + 1)
+        # month = str(cr.date.ymdhms['month'])
+        # if cr.date.ymdhms['day'] < 10:
+        #     day = '0' + str(cr.date.ymdhms['day'])
+        #     if cr.date.ymdhms['day'] < 9:
+        #         day2 = '0' + str(cr.date.ymdhms['day'] + 1)
+        # if cr.date.ymdhms['month'] < 10:
+        #     month = '0' + str(cr.date.ymdhms['month'])
+
+        # datestr = str(cr.date.ymdhms['year']) + '-' + month + '-' + day + '+' + day2
+        print(datestr)
+        self.mkwrk(cr)
+        # mk wrk folder, allow for it to already exist
+        # os.makedirs(wrkdir / datestr, exist_ok = True)
+        # os.makedirs(wrkdir / datestr / 'aligned', exist_ok = True)
+        # os.makedirs(wrkdir / datestr / 'calibrated', exist_ok = True)
+        # os.makedirs(wrkdir / datestr / 'uncalibrated', exist_ok = True)
+        if filters == None:
+            filters = cr.filters.keys()
+        
+        for filter in filters:
+            fildat = cr.data[cr.filters[filter]]
+            if (fildat.target == None):
+                fplate = str(int(cr.date.mjd)) + '-' + filter + '_'
+            else:
+                fplate = str(fildat.target.name) + '-' + filter + '_' 
+            if (fildat.calibrated == True) and (fildat.aligned == True): 
+                wrdir = wrkdir / datestr / 'aligned'
+                if len(filters) != 1:
+                    os.makedirs(wrkdir / datestr / 'aligned' / filter, exist_ok = True)
+                    wrdir = wrdir / filter
+                fsub = '_ca'
+            elif (fildat.calibrated == True):
+                wrdir = wrkdir / datestr / 'calibrated'
+                if len(filters) != 1:
+                    os.makedirs(wrkdir / datestr / 'calibrated' / filter, exist_ok = True)
+                    wrdir = wrdir / filter
+                fsub = '_c'
+            else: 
+                wrdir = wrkdir / datestr / 'uncalibrated'
+                if len(filters) != 1:
+                    os.makedirs(wrkdir / datestr / 'uncalibrated' / filter, exist_ok = True)
+                    wrdir = wrdir / filter
+                fsub = ''
+
+                
+            for p in range(len(fildat.data)):
+                image = fildat.data[p]
+                fname = fplate + str(p) + fsub + '.fits'
+                image.write(wrdir / fname, overwrite = True)
+
+    def saveWCS(self, cr, filters = None):
+        wrkdir = self.dordir / 'data' / 'wrk'
+        if cr.datestr == None:
+            self.getDateString(cr)
+        datestr = cr.datestr
+        self.mkwrk(cr)
+        if filters == None:
+            filters = cr.filters.keys()
+        
+        for filter in filters:
+            stack = cr.data[cr.filters[filter]]
+            if stack.wcs == None:
+                cr.getWCS(filter, self)
+            fname = str(filter) + '-solved.fits'
+            solved = cr.data[cr.filters[filter]].solved
+            solved.write(wrkdir / datestr / 'WCS' / fname, overwrite = True)
+            
+
+
+    # merge header
 
 
 
 
 
-clip = clippy()
-wasp33 = zellars('WASP33')
-cere = clip.mkceres('2020-11-04+05')
-# cere.calibrate('Red')
-cere.align('Red', clip)
-cere.dorphot('Red', wasp33)
+# we need to pass the calibration files(if none grab the most recent out of 
+# dorado by closest mjd)
+# if there is no calibration files in dorado send a warning and set 
+# calibration files to none
 
-wasp33.ts[wasp33.filters['Red']].show_in_browser(jsviewer = True)
-
-
-print('Finished')
-
-# save the calibration frames to dorado's directory (check if that one already exists)
-# we can compare these over time to look for its deviance
-
-# is this calibrated data? aligned?
-# extract the timestamps from each image
-
-# we need to pass the calibration files(if none grab the most recent out of dorado by closest mjd)
-# if there is no calibration files in dorado send a warning and set calibration files to none
+# make a function to force uint16
 
 
 
 
-
-
-
-# os.environ['HOME'] is also the home directory if needed
-# os.uname() for returning operating system information
-
-# os.open os.read os.write
-# os.get_terminal_size (gets columns, lines)
-
-# os.chdir for choosing current directory path
-# os.getcwd() get the current working directory
-# os.listdir(path='.') list directory contents by name
-
-# os.mkdir(path) make a directory
-# os.mkdirs(name, exist_ok = False) make directory and intermediate directories
-
-# os.remove(path) remove/delete a file path (not for directories)
-# os.rmdir(path) remove/delete a directory path (if dir is empty)
-# os.removedirs(name) recursively removes directories until it finds a non empty directory
-
-# os.rename(src, dst) rename a file or directory from src to dst
-# os.scandir(path='.') better than listdir, you can specify a file descriptor
-# with os.scandir(path) as it:
-#     for entry in it:
-#         if not entry.name.startswith('.') and entry.is_file():
-#             print(entry.name)
-
-# os.startfile(path[, operation]) acts like double clicking file in explorer to open with default
-# os.system(command) execute a system command string in subshell
-
-# getpass.getuser() similar to os.getlogin() gets username or login name. 
 

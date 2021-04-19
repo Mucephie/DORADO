e42361c51a0357329b638cc9474fa677aa494367
Joshua Parsons <mucephie@outlook.com>
2021-04-18 23:27:56 -0400

    Progress on docs

--- dorado/zellars/zellarsClass.py
+++ dorado/zellars/zellarsClass.py
@@ -9,6 +9,16 @@ import astropy.units as un
 __all__ = ['Zellars']
 
 class Zellars:
+    '''
+    The Zellars class represents an astronomical target and handles the targets releventt attributes.
+    Unpassed target parameters will be gathered via astroquery (SIMBAD).
+
+    Attributes
+    ----------
+
+    name: str
+        name of target in string format.
+    '''
     def __init__(self, name):
         # get it because zellars is the canadian target?
         self.name = name
@@ -22,6 +32,11 @@ class Zellars:
         # xy coordinates
         
     def calcmag(self, filter):
+        '''
+        calcmag converts the targets flux and associated uncertainty into an 
+        instrumental magnitude and uncertainty.
+
+        '''
         flux  = self.ts[self.filters[filter]]['flux']
         flux_unc = self.ts[self.filters[filter]]['flux_unc']
         magnitudes = -2.5 * np.log10(flux / 15)

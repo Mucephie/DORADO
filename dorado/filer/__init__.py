7f45f2e656ae994a15f23238c7b1d893316f498e
Joshua Parsons <mucephie@outlook.com>
2021-05-09 18:44:13 -0400

    fixed class name

--- dorado/filer/__init__.py
+++ dorado/filer/__init__.py
@@ -5,5 +5,5 @@ is just an example sub-package, so it doesn't actually do anything.
 """
 __all__ = []
 
-from .clippyClass import *
-__all__ += clippyClass.__all__
+from .filerClass import *
+__all__ += filerClass.__all__
